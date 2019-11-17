81addb2d14a19fff1a5f6aa78c83429e4a9a4edd
Quentin Sahal <quentin.sahal@gmail.com>
2019-11-17 12:40:15 +0100

    set the store structure with pseudo code

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-class MemoryCache {
-  constructor ({ ttl = 0, nspSeparator = '.' } = {}) {
-
-    this.memory = {}
-    this.lastModified = 0
-    this.ttl = ttl
-    this.nspSeparator = nspSeparator
-  }
-
-  clear = (nsp = '') => {
-    Object.keys(this.memory).forEach(key => {
-        const keyMatchNsp = key.split(this.nspSeparator)[0].match(nsp)
-        keyMatchNsp && delete this.memory[key]
-    })
-  }
-  get = (key) => this.memory[key]  
-  isExpired = () => this.lastModified + this.ttl > Date.now()
-  set = (name, content) => this.memory[name] = JSON.stringify(content)
-  updateLastModified = () => this.lastModified = Date.now()
-  
-}
-
 // Le rootStore est instancié une fois au chargement de l'app
 // il instancie tous les stores de l'app
 // Il permet aux stores de communiquer entre eux via sa réf
@@ -28,36 +6,23 @@ class MemoryCache {
 class RootStore {
   constructor (storeConfigs) {
     this.contractStore = new ContractStore()
-    this.optionStore = new OptionStore()
-
-    // Cache général au store
-    // faille: si une donnée secondaire est modifié 
-    // juste avant l'expiration, elle ne sera pas réellement cachée
-    this.cache = new storeConfigs.Cache()
-  }
-
-  // decorator qui permet de gérer le cache 
-  // sans s'en préoccuper lors d'un appel fetch
-  fetch = (name, useCache = false) => {
-    return async (target, key, descriptor) => {
-      const original = descriptor.value
-
-      descriptor.value = () => {
-        if(useCache && !this.cache.isExpired()) {
-          return this.cache.get(name)
-        }
-        if(this.cache.isExpired()) {
-          this.cache.clear(name)
-        }
-        const response = await original.apply(this, arguments)
-        this.cache.set(name, response)
-      }
-    }
+    this.optionsStore = new OptionsStore()
   }
 }
 
 class OptionsStore {
   id = Date.now()
+  
+  @observable isLoadingOptions = false
+  @observable isErrorOptions = false
+  @observable isErrorSubscription = false
+
+  // retour de la ressource
+  // Les clés seront les numéros de contrats
+  // Ces numéros de contrats contiendront un array d'options
+  // les observer vont-il s'updater si on ajoute une clé à l'objet
+  // A TESTER
+  @observable _sourceUserOptions = {}
 
   constructor (rootStore, service) {
     this.rootStore = rootStore
@@ -65,64 +30,25 @@ class OptionsStore {
 
   }
 
-  @observable isLoadingOptions = false
-  @observable isErrorOptions = false
+  @computed get contract () {
+    return this.rootStore.contractStore.currentContract
+  }
 
-  // retour de la ressource
-  // Les clés seront les numéros de contrats
-  // Ces numéros de contrats contiendront un array d'options
-  // ??? userOptions a t-il besoin d'etre un observable ???
-  @observable userOptions = {}
+  @computed get contractId () {
+    return this.contract.id
+  }
 
-  @computed get optionsAlreadyFetched ()) {
-    
+  @computed get optionsAlreadyFetched () {
+    return this._sourceUserOptions[this.contract.id] || this.isLoadingOptions
   }
 
   @computed get options () {
     if (!this.optionsAlreadyFetched) {
       this.fetchOptions()
+      return []
     }
-    return this.sourceOptions
+    return this._sourceUserOptions[this.contractId]
   }
-
-  // constitue la "Single Source Of Truth"
-  // ne doit pas etre utilisée directement 
-  @observable sourceOptions
-
-  // convention "<namespace>.<identifiant-unique>""
-  @this.fetch(`options.${this.id}`, false)
-  async fetchOptions = () => {
-    const contract = this.rootStore.contractStore.currentContract
-    this.setLoadingOptions(true)
-    this.setErrorOptions(false)
-    try {
-      const options = await optionsService.fetchOptions(contract)
-      this.setSourceOptions(options)
-    } catch (error) {
-      this.setErrorOptions(true)
-    }
-    this.setLoadingOptions(false)
-  }
-}
-
-// Tous ces Domain Stores peuvent hériter d'un Parent
-// responsable de l'installation d'un background commun 
-class ContractOptions {
-  constructor () {
-
-  }
-
-
-  
-  @action setSourceOptions = (options, contractId) => {
-    this.userOptions[contractId] = new ContractOptions(options)
-  }
-
-
-
-
-
-
 
   @action setLoadingOptions = (bool) => {
     this.isLoadingOptions = bool
@@ -131,37 +57,73 @@ class ContractOptions {
   @action setErrorOptions = (bool) => {
     this.isErrorOptions = bool
   }
+
+  @action setSourceOptions = (options) => {
+    if(!options) {
+      // allow us to reset the options for the current contract
+      // when a POST action is made (action that modifies the ressource)
+      // So we are guaranteed to fetch them again the next time we call options
+      delete this._sourceUserOptions[this.contractId]
+    }
+    this._sourceUserOptions[this.contractId] = options.map(opt => new Option(opt))
+  }
+
+  @action resetSourceOptions = () => {
+    this._sourceUserOptions = {}
+  }
+
+  @action subscribeOption = (optionId) => {
+    this.setErrorSubscription(false)
+    try {
+      await this.service.subscribeOption(optionId, this.contract)
+      this.setSourceOptions(null)
+    } catch (error) {
+      this.setErrorSubscription(true)
+    }
+  }
+
+  @action setErrorSubscription = (bool) => {
+    this.isErrorSubscription = bool
+  }
+
+  @action resetStore () {
+    // reset all the value 
+    // Pourrait peut etre appartenir au rootStore
+    // Ou reset tous les observables
+    this.resetSourceOptions()
+    this.setErrorOptions(false)
+    this.setErrorSubscription(false)
+    this.setLoadingOptions(false)
+
+  }
+
+  async fetchOptions = () => {
+    this.setLoadingOptions(true)
+    this.setErrorOptions(false)
+    try {
+      const options = await this.service.fetchOptions(this.contract)
+      this.setSourceOptions(options)
+    } catch (error) {
+      this.setErrorOptions(true)
+    }
+    this.setLoadingOptions(false)
+  }
 }
 
 class Option {
   constructor (option) {
     // objet option retourné de la ressource
     // ne doit pas etre utilisé directement
-    this.sourceOption = option
-    this.isErrorSubscription = false
+    this._sourceOption = option
   }
 
   @computed get label () {
-    return this.option.libelle
+    return this._sourceOption.libelle
   }
 
   @computed get isSubscribed () {
-    return this.option.subscribed
+    return this._sourceOption.subscribed
   }
   // ... on peut imaginer tout un tas de computed
   // tirés directement de la source
-
-  @action setErrorSubscription = (bool) => {
-    this.isErrorSubscription = bool
-  }
-
-  @action subscribe = () => {
-    this.setErrorSubscription(false)
-    try {
-      await optionsService.subscribeOption(contract)
-    } catch (error) {
-      this.setErrorSubscription(true)
-    }
-    
-  }
 }
